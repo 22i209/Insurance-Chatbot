04f47c0088f4808be2238e6a1b575cb09ddac679
22i209 <166497047+22i209@users.noreply.github.com>
2025-01-26 19:41:55 +0530

    Add files via upload

---
+++ Avatar.js
@@ -0,0 +1,10 @@
+// Avatar.js
+import React from 'react';
+import '../App.css'; // Going up one level to the src folder
+
+const Avatar = (props) => {
+  return (
+    <div className='ADE'>ADE</div>
+  );
+};
+export default Avatar;
---
+++ data.js
@@ -0,0 +1,42 @@
+const data ={
+    kid: {
+        slow: {
+            name: "basic Child Plan",
+            imageUrl: "https://static.pbcdn.in/cdn/images/bu/investment/triple-benefits-of-child-plan-mobile.png",
+            link:"https://static.pbcdn.in/cdn/images/bu/investment/triple-benefits-of-child-plan-mobile.png"
+        },
+        fast: {
+            name: "Premium Child Insurance",
+            imageUrl: "https://static.pbcdn.in/cdn/images/bu/investment/triple-benefits-of-child-plan-mobile.png",
+            link:"https://static.pbcdn.in/cdn/images/bu/investment/triple-benefits-of-child-plan-mobile.png"
+    },
+    teenagers: {
+        slow: {
+            name: "Basic Education Insurance",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaA0MgUV1LWBWu03yhDY8Wk9OSa_C5dU8CRg&s",
+            link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaA0MgUV1LWBWu03yhDY8Wk9OSa_C5dU8CRg&s"
+        },
+        fast: {
+            name: "Masters Education Insurance",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaA0MgUV1LWBWu03yhDY8Wk9OSa_C5dU8CRg&s",
+            link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaA0MgUV1LWBWu03yhDY8Wk9OSa_C5dU8CRg&s"
+           
+        }
+    },
+    adults: {
+        slow: {
+            name: "Personal Insurance",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFP55yFo8m1LEqZmgK1rtU33TgvL_U9PaZdw&s",
+            link:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFP55yFo8m1LEqZmgK1rtU33TgvL_U9PaZdw&s"
+           
+        fast: {
+            name: "Health Insurance",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzMYmdywLiyYnVT56PrIuQnkHKaS11ikUiA&s",
+            link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzMYmdywLiyYnVT56PrIuQnkHKaS11ikUiA&s"
+        }
+    }
+
+    }
+}
+
+export default data;
